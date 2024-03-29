ec35680846e82f75011374c6e0cdc94cf7e40000
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-03-29 21:02:37 +0000

    GitHub Action for Pages build

--- Site-Builder/updateWorkspace.js
+++ Site-Builder/updateWorkspace.js
@@ -2,11 +2,24 @@
 	Updates the top level package.json's workspace to include all the folders that are built
 */
 
-import { fileURLToPath } from "url";
+const PLACEHOLDER = `"PLACEHOLDER"`;
+
+import fs from "fs-extra";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import config from "./config.js";
 
 process.chdir(path.dirname(fileURLToPath(import.meta.url)));
 
-console.log(config);
+const pkgJSONTemplate = await fs.readFile("static/top-package-template.json", { encoding: "utf8" });
+if (! pkgJSONTemplate.includes(PLACEHOLDER)) {
+	throw new Error("Couldn't find PLACEHOLDER");
+}
+
+const workspaceValue = JSON.stringify([
+	...config.build
+]);
+const newPackageJSON = pkgJSONTemplate.replace(PLACEHOLDER, workspaceValue);
+
+await fs.writeFile("../package.json", newPackageJSON, { encoding: "utf8" });
