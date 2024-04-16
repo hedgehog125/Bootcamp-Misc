9260488fe93d288a758c1d2cffc93d2429ff455d
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-04-16 16:02:34 +0100

    Added a page that gets the average colour of an XKCD comic

---
+++ Average-XKCD-Colour/assets/main.js
@@ -0,0 +1,58 @@
+const XKCD_API_URL = "https://xkcd.com/";
+const CORS_PROXY_URL = "https://corsproxy.io/";
+
+const corsProxy = (url) => `${CORS_PROXY_URL}?${encodeURIComponent(url)}`;
+
+document.querySelector("#avgForm").addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const submitEl = document.querySelector("#submit");
+	submitEl.disabled = true;
+
+	const formData = new FormData(e.target);
+	const url = `${XKCD_API_URL}${formData.get("id")}/info.0.json`;
+	const res = await (await fetch(corsProxy(url))).json();
+
+	const img = new Image();
+	img.crossOrigin = "anonymous";
+	img.src = corsProxy(res.img);
+	await new Promise((resolve, reject) => {
+		img.onload = () => {
+			resolve();
+		};
+		img.onerror = () => {
+			reject();
+		};
+	});
+
+	const canvas = document.createElement("canvas");
+	canvas.width = img.width;
+	canvas.height = img.height;
+	const ctx = canvas.getContext("2d", { willReadFrequently: true });
+	ctx.drawImage(img, 0, 0);
+
+	const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	let total = [0, 0, 0];
+	for (let i = 0; i < imgData.data.length; i++) {
+		total[i % 4] += imgData.data[i];
+	}
+
+	const imgContainer = document.querySelector("#imageContainer");
+	imgContainer.innerHTML = "";
+	imgContainer.appendChild(img);
+
+	document.querySelector(
+		"#averageSentence"
+	).innerText = `The average colour is `;
+
+	const avg = total
+		.slice(0, 3)
+		.map((value) => Math.round(value / (imgData.data.length / 4)));
+
+	const rgb = `rgb(${avg.join(", ")})`;
+	const avgValEl = document.querySelector("#averageValue");
+	avgValEl.innerText = rgb;
+	avgValEl.style.color = rgb;
+
+	document.querySelector("#output").hidden = false;
+	submitEl.disabled = false;
+});
