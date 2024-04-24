856b8aa9080fe8ff67419f694987dd585494f22b
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-04-24 20:09:17 +0100

    Added some debug logs to the builder

--- Site-Builder/index.js
+++ Site-Builder/index.js
@@ -12,17 +12,23 @@ import { fileURLToPath } from "url";
 const exec = promisify(execCallback);
 process.chdir(path.dirname(fileURLToPath(import.meta.url)));
 
-await fs.emptyDir(BUILD_DIR);
+console.log("Clearing build...");
+const clearPromise = fs.emptyDir(BUILD_DIR);
+clearPromise.then(() => {
+	console.log(`Cleared ${BUILD_DIR}`);
+});
+await clearPromise;
 
 console.log("Building...");
 
-
 await Promise.all([
-	...config.static.map(async dirName => {
+	...config.static.map(async (dirName) => {
+		console.log(`Making ${dirName}`);
 		await fs.mkdir(path.join(BUILD_DIR, dirName));
+		console.log(`Made ${dirName}`);
 		await fs.copy(path.join("..", dirName), path.join(BUILD_DIR, dirName));
 	}),
-	...config.build.map(async dirName => {
+	...config.build.map(async (dirName) => {
 		const cwd = process.cwd();
 		process.chdir(path.join("..", dirName));
 		const buildTask = exec("npm run build");
@@ -32,24 +38,36 @@ await Promise.all([
 			throw new Error(`${dirName} build failed. Error:\n${stderr}`);
 		}
 
+		console.log(`Making ${dirName}`);
 		await fs.mkdir(path.join(BUILD_DIR, dirName));
-		await fs.copy(path.join("..", dirName, "build"), path.join(BUILD_DIR, dirName));
+		console.log(`Made ${dirName}`);
+		await fs.copy(
+			path.join("..", dirName, "build"),
+			path.join(BUILD_DIR, dirName)
+		);
 	}),
 	(async () => {
 		await fs.copy("static/index", path.join(BUILD_DIR));
 
-
-		const indexTemplate = await fs.readFile("static/index-template.html", { encoding: "utf8" });
-		if (! indexTemplate.includes(INDEX_PLACEHOLDER)) {
-			throw new Error("Index template doesn't contain INDEX_PLACEHOLDER.");
+		const indexTemplate = await fs.readFile("static/index-template.html", {
+			encoding: "utf8",
+		});
+		if (!indexTemplate.includes(INDEX_PLACEHOLDER)) {
+			throw new Error(
+				"Index template doesn't contain INDEX_PLACEHOLDER."
+			);
 		}
 
 		const links = [...config.static, ...config.build];
-		const indexValue = `<ul>${links.map(dirName => `<li><a href="${dirName}/">${dirName}</a></li>`).join("\n")}</ul>`;
+		const indexValue = `<ul>${links
+			.map((dirName) => `<li><a href="${dirName}/">${dirName}</a></li>`)
+			.join("\n")}</ul>`;
 		const newIndex = indexTemplate.replace(INDEX_PLACEHOLDER, indexValue);
-		
-		await fs.writeFile(path.join(BUILD_DIR, "index.html"), newIndex, { encoding: "utf8" }); 
-	})()
+
+		await fs.writeFile(path.join(BUILD_DIR, "index.html"), newIndex, {
+			encoding: "utf8",
+		});
+	})(),
 ]);
 
 console.log("Done");
