95e2d3b9e2f18132c24483601250a7c73607bb82
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-05-01 13:41:26 +0100

    Finished React Router test

--- React-Router/src/app/App.js
+++ React-Router/src/app/App.js
@@ -27,8 +27,14 @@ const router = createBrowserRouter(
 				<Route path="about" element={<About />}></Route>
 				<Route path="sign-up" element={<SignUp />}></Route>
 				<Route path="articles" element={<Articles />}></Route>
-				<Route path="categories" element={<Categories />}></Route>
-				<Route path="profile" element={<Profile />}></Route>
+				<Route path="articles/:title" element={<Article />}></Route>
+				<Route path="categories" element={<Categories />}>
+					<Route path=":name" element={<Category />} />
+				</Route>
+				<Route path="profile" element={<Profile />}>
+					<Route path="edit" element={<EditProfileForm />}></Route>
+				</Route>
+				<Route path="authors/:name" element={<Author />}></Route>
 			</Route>
 		</>
 	)
---
+++ TODO.md
@@ -0,0 +1,3 @@
+# Bugs
+
+Add correct base URL to the React apps. Provide as environment variable from Site-Builder
