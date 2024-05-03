f19aee91f37d376c255bc1d241ce25f0c85cc5cb
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-05-03 14:59:23 +0100

    Reorganised the contact form

--- Appointment-Planner/src/components/contactForm/ContactForm.jsx
+++ Appointment-Planner/src/components/contactForm/ContactForm.jsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-export function ContactForm({
-	name,
-	setName,
-	phone,
-	setPhone,
-	email,
-	setEmail,
-	handleSubmit,
-}) {
-	return <></>;
+export function ContactForm({ onSubmit }) {
+	const [nameInput, setNameInput] = useState("");
+	const [phoneNumInput, setPhoneNumInput] = useState("");
+	const [emailInput, setEmailInput] = useState("");
+
+	return (
+		<form action="#" onSubmit={handleSubmit}>
+			<label>
+				Name:
+				<input
+					type="text"
+					name="name"
+					value={nameInput}
+					onChange={({ target }) => setNameInput(target.value)}
+				/>
+			</label>
+			<br />
+			<label>
+				Phone Number:
+				<input
+					type="tel"
+					name="phone-number"
+					value={phoneNumInput}
+					onChange={({ target }) => setPhoneNumInput(target.value)}
+				/>
+			</label>
+			<br />
+			<label>
+				Email:
+				<input
+					type="email"
+					name="email"
+					value={emailInput}
+					onChange={({ target }) => setEmailInput(target.value)}
+				/>
+			</label>
+			<br />
+			<button type="submit">Submit</button>
+		</form>
+	);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		const succeeded = onSubmit({
+			name: nameInput,
+			phoneNum: phoneNumInput,
+			email: emailInput,
+		});
+		if (succeeded) {
+			setNameInput("");
+			setPhoneNumInput("");
+			setEmailInput("");
+		}
+	}
 }
--- Appointment-Planner/src/containers/contactsPage/ContactsPage.jsx
+++ Appointment-Planner/src/containers/contactsPage/ContactsPage.jsx
@@ -1,56 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
 export function ContactsPage({ contacts, onSubmit }) {
-	const [nameInput, setNameInput] = useState("");
-	const [phoneNumInput, setPhoneNumInput] = useState("");
-	const [emailInput, setEmailInput] = useState("");
-
 	return (
 		<div>
 			<section>
 				<h2>Add Contact</h2>
-				<form action="#" onSubmit={handleSubmit}>
-					<label>
-						Name:
-						<input
-							type="text"
-							name="name"
-							value={nameInput}
-							onChange={({ target }) =>
-								setNameInput(target.value)
-							}
-						/>
-					</label>
-					<br />
-					<label>
-						Phone Number:
-						<input
-							type="tel"
-							name="phone-number"
-							value={phoneNumInput}
-							onChange={({ target }) =>
-								setPhoneNumInput(target.value)
-							}
-						/>
-					</label>
-					<br />
-					<label>
-						Email:
-						<input
-							type="email"
-							name="email"
-							value={emailInput}
-							onChange={({ target }) =>
-								setEmailInput(target.value)
-							}
-						/>
-					</label>
-					<br />
-					<button type="submit">Submit</button>
-				</form>
+				<ContactForm onSubmit={onSubmit} />
 			</section>
 			<hr />
 			<section>
@@ -58,18 +16,4 @@ export function ContactsPage({ contacts, onSubmit }) {
 			</section>
 		</div>
 	);
-
-	function handleSubmit(e) {
-		e.preventDefault();
-		const succeeded = onSubmit({
-			name: nameInput,
-			phoneNum: phoneNumInput,
-			email: emailInput,
-		});
-		if (succeeded) {
-			setNameInput("");
-			setPhoneNumInput("");
-			setEmailInput("");
-		}
-	}
 }
